a524d9d7bb1d3d7bf2a9aa64d8a57ac90c3ccf60
Venomorous <94299601+Venomorous@users.noreply.github.com>
2024-02-13 11:12:33 +0200

    Refactor RandomSteppedArray methhod

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -49,10 +49,11 @@ public class Lab1
     {
         int[][] array = new int[n][];
         Random random = new Random();
-
+        int k;
         for (int i = 0; i < n; i++)
         {
-            array[i] = new int[i + 1];
+            k = random.Next(1, n);
+            array[i] = new int[k];
         }
 
         for (int i = 0; i < n; i++)
